edb59d4d763c6493225e2f34cb781ba980183ea5
Maxi <maxirivarola354@gmail.com>
2026-06-30 21:15:26 -0300

    feat: add swagger documentation

---
+++ src/main/java/com/cafemanager/cafemanager/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.cafemanager.cafemanager.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OpenApiConfig {
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("CafeManager API")
+                                .version("1.0")
+                                .description(
+                                        "API para gestión de cafetería: " +
+                                                "productos, ventas, compras, stock y pérdidas"
+                                )
+                );
+    }
+
+}
